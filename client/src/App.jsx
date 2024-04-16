063456e57b62df22169bb059fbf00656254d23c8
Debayan <debayanghosh408@gmail.com>
2024-04-17 01:33:31 +0530

    added some stylo change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -75,7 +75,7 @@ export default function App() {
         <div className="fixed bottom-4 right-4">
           <button
             className={`bg-blue-500 text-white px-4 py-2 rounded cursor-pointer shadow ${
-              chatOpen ? "rounded-t-lg" : "rounded-lg"
+              chatOpen ? "rounded-t-lg" : "rounded-t-lg"
             }`}
             onClick={toggleChat}
           >
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -5,7 +5,7 @@ export default function Main() {
   const {currentUser} = useSelector ((state)=> state.user);
   return (
     <div className=''>
-    fhvkjewfj
+    <h1 className='font-bold text-3xl text-center'>Welcome to Brainware University</h1>
  
     
     

6e47159d2be66e2c0d5208d98f11833af3eca921
Debayan <132012594+felixoder@users.noreply.github.com>
2024-04-19 01:19:33 +0530

    Update CreateToppers.jsx

--- client/src/Toppers/CreateToppers.jsx
+++ client/src/Toppers/CreateToppers.jsx
@@ -184,7 +184,7 @@ export default function CreateToppers() {
             onChange={(e) => setFile(e.target.files[0])}
           />
           <Button
-          className="-z-20"
+          
             type="button"
             gradientDuoTone="purpleToPink"
             outline
@@ -211,17 +211,13 @@ export default function CreateToppers() {
             className="w-72 h-72 object-cover mx-auto"
           />
         )}
-        <Button
-        
-          gradientDuoTone="purpleToBlue"
-          className="-z-20 mb-3"
-          pill
-          size="sm"
-          outline
+        <button
+          className=" mb-3 bg-blue-800 text-white rounded-md font-semibold h-[50px]"
+          
           type="submit"
         >
           Create
-        </Button>
+        </button>
       </form>
     </div>
   );
